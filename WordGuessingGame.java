3f16b2627ce65dfeb9ca73efd9ede0cff0f4d31e
NinjaGameDev <202100631@estudantes.ips.pt>
2024-03-13 10:00:50 +0000

    Nivel 2 Alinea 3

--- WordGuessingGame.java
+++ WordGuessingGame.java
@@ -38,5 +38,45 @@ public class WordGuessingGame
     public void showGuessedWord(){
         System.out.println("Guessed Word: " + guessedWord);
     }
+    
+    private void showWelcome(){
+        System.out.println("Welcome to the world guessing game");
+    }
+    
+    private String changeCharInPosition(int pos, char c, String s){
+        char[] charArray = s.toCharArray();
+        charArray[pos] = c;
+        return new String(charArray);
+    }
+    
+    private boolean guess(char letter){
+        int i = hiddenWord.indexOf(letter);
+        boolean result = false;
+        while (i >= 0){
+            guessedWord = changeCharInPosition(i, letter, guessedWord);
+            i = hiddenWord.indexOf(letter,i+1);
+            result = true;
+        }
+        return result;
+    }
+    
+    private void showResult(){
+        System.out.println("A palavra foi encontrada com " +numberOfTries+ " tentativas");
+    }
 
+    
+    public void play(){ //Nivel 2
+        while (!guessedWord.equals(hiddenWord)){
+            showWelcome();
+            showGuessedWord();
+            if (guess(reader.getChar("What letter is in the word?: "))){
+                System.out.println("A letra estava na palavra");
+            }else{
+                System.out.println("A letra não estava na palavra");
+            }
+            System.out.println(guessedWord);
+            numberOfTries++;
+        }
+        showResult();
+    }
 }
