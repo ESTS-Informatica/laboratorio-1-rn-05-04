9fc2bfd9f665e3eb6cb257a87333196186b9dc79
rafa.cavalinhos <202002107@estudantes.ips.pt>
2024-03-13 09:32:27 +0000

    Nível 3 - Alínea 2

--- WordGenerator.java
+++ WordGenerator.java
@@ -1,7 +1,27 @@
 import java.util.ArrayList;
 
 public class WordGenerator {
-    private ArrayList<String> words = new ArrayList<String>();
+    ArrayList<String> words;
     
+    public WordGenerator() {
+        words = new ArrayList<String>();
+    }
     
+    public void fillArrayList() {
+        words.add("boolean");
+        words.add("break");
+        words.add("byte");
+        words.add("case");
+        words.add("char");
+        words.add("class");
+        words.add("continue");
+        words.add("do");
+        words.add("double");
+        words.add("else");
+        words.add("enum");
+        words.add("for");
+        words.add("if");
+        words.add("import");
+        words.add("int");
+    }
 }
